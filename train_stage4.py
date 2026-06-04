0a864f79244f3ab5c8266b955d19cc54c6015e6d
Aishwarya-Pragada <aishwaryapragada31@gmail.com>
2026-06-04 18:14:55 +0530

    Update train_stage4.py

--- train_stage4.py
+++ train_stage4.py
@@ -4,7 +4,7 @@ from stable_baselines3.common.callbacks import (
     BaseCallback
 )
 
-from rl_env_stage33 import A1Env
+from rl_env_stage4 import A1Env
 
 
 class CurriculumCallback(BaseCallback):
@@ -25,15 +25,15 @@ class CurriculumCallback(BaseCallback):
 
 env = A1Env()
 
-model = PPO.load("stage32_trot_3M", env=env)
+model = PPO.load("stage3_trot_3M", env=env)
 
-print("Loaded stage32_trot_3M successfully.")
-print("Continuing training with stage33 — force_symmetry_penalty increased.")
+print("Loaded stage3_trot_3M successfully.")
+print("Continuing training with stage4 — force_symmetry_penalty increased.")
 
 checkpoint_callback = CheckpointCallback(
     save_freq=100_000,
     save_path="./checkpoints/",
-    name_prefix="stage33_trot"
+    name_prefix="stage4_trot"
 )
 
 curriculum_callback = CurriculumCallback(
@@ -46,7 +46,7 @@ model.learn(
     reset_num_timesteps=False   # continue counter from 2M
 )
 
-model.save("stage33_trot_4M")
+model.save("stage4_trot_4M")
 
-print("Stage 33 training completed.")
-print("Saved to: stage33_trot_4M")
+print("Stage 4 training completed.")
+print("Saved to: stage4_trot_4M")
